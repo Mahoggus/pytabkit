b3a93a3da0304aef7fbfe0af80f7cfaa7d721fe3
Mahoggus <lekynam02@gmail.com>
2025-10-18 15:04:51 +0700

    Update metrics.py

--- pytabkit/models/training/metrics.py
+++ pytabkit/models/training/metrics.py
@@ -79,7 +79,7 @@ def mse(y_pred, y, reduction='mean'):
     res = ((y_pred - y) ** 2).mean(dim=-1)
     return apply_reduction(res, reduction)
 
-def l1_loss(y_pred, y, beta=0.5, reduction='mean'):
+def smooth_l1_loss(y_pred, y, beta=0.5, reduction='mean'):
     if not torch.is_floating_point(y):
         # in case mse should be used for classification
         y = F.one_hot(y.squeeze(-1), num_classes=y_pred.shape[-1])
